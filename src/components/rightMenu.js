0f87bce937e0af1f5f4fa6068cb63f0eb8c90928
DanCLee <dan.clee1218@gmail.com>
2016-09-07 03:49:31 -0400

    styled inputs with display:table

--- src/components/rightMenu.js
+++ src/components/rightMenu.js
@@ -32,31 +32,92 @@ class RightMenu extends Component {
 				<div className="main">
 					<div className="filters">
 
-						<div className="categories marg">
-							<h4> Categories </h4>
-							<ul>
-								<li className="rows" > 
-									{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
-									<p className=""> Serif </p>
-								</li>
-								<li className="rows" > 
-									{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
-									<p className=""> Sans Serif </p>
-								</li>
-								<li className="rows" > 
-									{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
-									<p className=""> Display </p>
-								</li>
-								<li className="rows" > 
-									{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
-									<p className=""> Handwriting </p>
-								</li>
-								<li className="rows" > 
-									{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
-									<p className=""> Monospace </p>
-								</li>
-							</ul>
+							<div className="marg">
+
+								<div className="categories">
+									<h4> Categories </h4>
+									<ul>
+										<li> 
+											{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
+											<p className=""> Serif </p>
+										</li>
+										<li> 
+											{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
+											<p className=""> Sans Serif </p>
+										</li>
+										<li> 
+											{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
+											<p className=""> Display </p>
+										</li>
+										<li  > 
+											{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
+											<p className=""> Handwriting </p>
+										</li>
+										<li  > 
+											{ this.state.serif ?  <i className="material-icons primaryColor">check_box</i>  : <i className="material-icons sections">check_box_outline_blank</i> }
+											<p className=""> Monospace </p>
+										</li>
+									</ul>
+								</div>
+
+
+								<div className="sorting">
+									
+									<ul>
+										<li>
+											<h4> Number of styles </h4>
+											<div className="rangeOption">
+												<div className="input">
+													<i className="material-icons">check_box_outline_blank</i>
+												</div>
+												<div className="input ranger">
+													<input type="range" name="pixels" min="0" max="50"  />
+												</div>
+											</div>
+
+										</li>
+										<li>
+											<h4> Thickness </h4>
+											<div className="rangeOption">
+												<div className="input">
+													<i className="material-icons">check_box_outline_blank</i>
+												</div>
+												<div className="input ranger">
+													<input type="range" name="pixels" min="0" max="50"  />
+												</div>
+											</div>
+
+										</li>
+										<li>
+											<h4> Slant </h4>
+											<div className="rangeOption">
+												<div className="input">
+													<i className="material-icons">check_box_outline_blank</i>
+												</div>
+												<div className="input ranger">
+													<input type="range" name="pixels" min="0" max="50"  />
+												</div>
+											</div>
+
+										</li>
+										<li>
+											<h4> Width </h4>
+											<div className="rangeOption">
+												<div className="input">
+													<i className="material-icons">check_box_outline_blank</i>
+												</div>
+												<div className="input ranger">
+													<input type="range" name="pixels" min="0" max="50"  />
+												</div>
+											</div>
+
+										</li>
+									</ul>
+								</div>
+
+
 						</div>
+
 					</div>
 
 				</div>
